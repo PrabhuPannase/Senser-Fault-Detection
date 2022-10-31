7a02dd5e725dc9d03556f9203f811fd35598064b
PrabhuPannase <114793576+PrabhuPannase@users.noreply.github.com>
2022-11-01 02:12:53 +0530

    work hard

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='Prabhu',
+    author_email='prabhupannase7@gmail.com',
+    packages= find_packages(),
+    install_requires= []
+
+)
