93942aacc4eb3e8991faa84d0c66d29c76826c74
PrabhuPannase <114793576+PrabhuPannase@users.noreply.github.com>
2022-11-11 22:20:15 +0530

    create structure of project

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements()-> List[str]:
+    '''Returns a list of requirements'''
+    requirement_list = []
+    return requirement_list
 
 setup(
     name='sensor',
@@ -6,6 +12,6 @@ setup(
     author='Prabhu',
     author_email='prabhupannase7@gmail.com',
     packages= find_packages(),
-    install_requires= []
+    install_requires= get_requirements()
 
 )
